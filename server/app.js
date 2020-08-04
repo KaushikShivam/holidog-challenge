cc2ca7d800b2001cb24159205279e5296dbb7872
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-08-04 15:58:57 +0530

    Add user auth controller actions

--- server/app.js
+++ server/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // Global middlewares
 app.use(express.json());
-app.use(cookieParser());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
---
+++ server/models/userModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: [true, 'This email already exists'],
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'Password must be atleast 8 characters long'],
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords do not match!',
+    },
+  },
+});
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (
+  providedPassword,
+  userPassword
+) {
+  return await bcrypt.compare(providedPassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
