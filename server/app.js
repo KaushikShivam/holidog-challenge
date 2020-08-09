05d4fdf7a2c726d08f76c38c19c482543d28e57d
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-08-09 20:12:26 +0530

    Add config for deployment

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const compression = require('compression');
+const path = require('path');
 
 const CustomError = require('./utils/CustomError');
 const globalErrorHandler = require('./utils/globalErrorHandler');
@@ -48,6 +50,15 @@ app.use(xss());
 
 // Mount api router
 app.use('/api/v1', apiRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(compression());
+  app.use(express.static('client/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 // Unhandled routes
 app.all('*', (req, res, next) => {
